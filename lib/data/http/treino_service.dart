b7c37821676b176d048a044fe9b71725c1c4afce
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2024-06-09 17:02:15 -0300

    lógica de adicionar treino

--- lib/data/http/treino_service.dart
+++ lib/data/http/treino_service.dart
@@ -7,6 +7,7 @@ class TreinoService {
   static const String baseUrl = 'http://10.0.2.2:8080';
   static const String treinoEndpoint = '/treino/lista';
   static const String listarExercicios = '/treino/listar_exercicio/';
+  static const String cadastrarTreino = '/treino';
 
   static Future<List<TreinoModel>> fetchTreinos() async {
     final url = Uri.parse('$baseUrl$treinoEndpoint');
@@ -21,7 +22,7 @@ class TreinoService {
     }
   }
 
-    static Future<List<TreinoComExerciciosModel>> getTreinoAndExercicios(String treinoId) async {
+  static Future<List<TreinoComExerciciosModel>> getTreinoAndExercicios(String treinoId) async {
     final url = Uri.parse('$baseUrl$listarExercicios$treinoId');
     final response = await http.get(url);
 
@@ -32,6 +33,32 @@ class TreinoService {
       return [treino]; 
     } else {
       throw Exception('Falha ao carregar treinos: ${response.statusCode}');
+    }
+  }
+
+ static Future<TreinoModel> addTreino(String nome, String diaSemana) async {
+    final url = Uri.parse('$baseUrl$cadastrarTreino');
+    
+    final Map<String, dynamic> requestBody = {
+      'nome': nome,
+      'diaSemana': diaSemana,
+    };
+
+    final response = await http.post(
+      url,
+      body: jsonEncode(requestBody),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final utf8Response = utf8.decode(response.bodyBytes);
+      final data = jsonDecode(utf8Response) as Map<String, dynamic>;
+      final treino = TreinoModel.fromMap(data);
+      return treino; 
+    } else {
+      throw Exception('Falha ao cadastrar treino: ${response.statusCode}');
     }
   }
 }
--- lib/screens/treino_add.dart
+++ lib/screens/treino_add.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_traintrax/components/decoration_input.dart';
-import 'package:flutter_traintrax/data/models/exercicio_model.dart';
-import 'package:flutter_traintrax/data/models/treino_model.dart';
+import 'package:flutter_traintrax/data/http/treino_service.dart';
+import 'package:flutter_traintrax/screens/home.dart';
 
 class TreinoAdd extends StatefulWidget {
   TreinoAdd({Key? key}) : super(key: key);
@@ -11,168 +11,136 @@ class TreinoAdd extends StatefulWidget {
 }
 
 class _TreinoAddState extends State<TreinoAdd> {
-  final TreinoModel treinoModel = TreinoModel(
-    id: "1", 
-    nome: "nome", 
-    diaSemana: "segunda"
-  );
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nomeController = TextEditingController();
+  final TextEditingController _diaSemanaController = TextEditingController();
+  bool _showTreinoInfo = false;
+  bool _showForm = true;
+  String _nomeTreino = '';
+  String _diaSemana = '';
 
-  final List<ExercicioModel> listaExercicios = [
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-    ExercicioModel(id: "id", nome: "nome", series: "series", repeticoes: "repeticoes"),
-  ];
+  void _adicionarTreino() async {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Processing Data')),
+      );
+
+      try {
+        final treino = await TreinoService.addTreino(
+          _nomeController.text,
+          _diaSemanaController.text,
+        );
+
+        setState(() {
+          _showTreinoInfo = true;
+          _showForm = false; // Oculta o formulário após adicionar o treino
+          _nomeTreino = treino.nome;
+          _diaSemana = treino.diaSemana;
+        });
+      } catch (e) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro ao adicionar treino: $e')),
+        );
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text(
           "Treino",
           style: TextStyle(color: Colors.white),
         ),
-        backgroundColor: Color(0xFF7600F5),
+        backgroundColor: const Color(0xFF7600F5),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          child: Column (
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              TextFormField(
-                decoration:getInput("Nome do Treino")
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                decoration:getInput("Dia da Semana")
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {},
-                  style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF7600F5)), 
-                ),
-                child: const Text(
-                  'Adicionar Treino',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18 
-                ),
-                )
-              ),
-              ElevatedButton(
-                onPressed: () {
-                   Navigator.pushNamed(
-                    context,
-                    '/exercicioAdd',
-                    arguments: {},
-                  );
-                },
-                  style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(Color(0xFF7600F5)), 
-                ),
-                child: const Text(
-                  'Adicionar Exercício',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18 
-                ),
-                )
-              ),
-             SizedBox(height: 16),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: listaExercicios.length,
-                  itemBuilder: (context, index) {
-                    ExercicioModel exercicioModel = listaExercicios[index];
-                    return Column(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.grey[200], 
-                            borderRadius: BorderRadius.circular(10.0), 
-                          ),
-                          child: ListTile(
-                               contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
-                            title: Text(exercicioModel.nome),
-                            subtitle: Text("${exercicioModel.series} - ${exercicioModel.repeticoes}"),
-                            trailing: IconButton(
-                              icon: const Icon(
-                                Icons.edit, 
-                                color: Colors.purple, 
-                              ),
-                               onPressed: () {
-                                Navigator.pushNamed(
-                                  context, '/exercicioEdit');
-                              },
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16), 
-                      ],
-                    );
-                  },
-                ),
-              ),
-            ]
-          ),
-        ),
+        padding: const EdgeInsets.all(16.0),
+        child: _showForm ? _buildForm() : _buildTreinoInfo(),
       ),
     );
   }
-}
 
-class BoxWithTitleAndSubtitle extends StatelessWidget {
-  final String title;
-  final String subtitle;
-
-  const BoxWithTitleAndSubtitle({
-    required this.title,
-    required this.subtitle,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child:Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          TextFormField(
+            controller: _nomeController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Campo obrigatório.';
+              }
+              return null;
+            },
+            decoration: getInput("Nome do Treino"),
+          ),
+          const SizedBox(height: 16),
+          TextFormField(
+            controller: _diaSemanaController,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Campo obrigatório.';
+              }
+              return null;
+            },
+            decoration: getInput("Dia da Semana"),
+          ),
+          const SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: _adicionarTreino,
+            style: ButtonStyle(
+              backgroundColor:
+                  WidgetStateProperty.all<Color>(const Color(0xFF7600F5)),
+            ),
+            child: const Text(
+              'Adicionar Treino',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
             ),
           ),
-          const SizedBox(height: 10),
-          Text(
-            subtitle,
-            style: const TextStyle(
-              fontSize: 16,
-            ),
-          ),
+          const SizedBox(height: 16),
         ],
       ),
     );
   }
+
+  Widget _buildTreinoInfo() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+      Row(
+        children: [
+          Expanded(
+            child: Text(
+              _nomeTreino, 
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.edit,
+              color: Colors.purple[700],
+            ),
+            iconSize: 30,
+            onPressed: () {
+              Navigator.pushNamed(context, '/treinoEdit');
+            },
+          ),
+        ],
+      ),
+      const SizedBox(height: 8.0),
+      Text(
+        _diaSemana, 
+        style: TextStyle(fontSize: 16),
+      ),
+    ],
+  );
+}
+
 }
